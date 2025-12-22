0f03389535ba6919773b110a940c079eae5a3587
caio20538 <caio20538@gmail.com>
2025-12-22 12:24:20 -0300

    feat: add default layout style

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,15 +1,19 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ɵEmptyOutletComponent } from "@angular/router";
 
 @Component({
   selector: 'app-default-login-layout',
   standalone: true,
   imports: [
-    NgOptimizedImage
-  ],
+    NgOptimizedImage,
+    ɵEmptyOutletComponent
+],
   templateUrl: './default-login-layout.component.html',
   styleUrl: './default-login-layout.component.scss'
 })
 export class DefaultLoginLayoutComponent {
-
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
 }
